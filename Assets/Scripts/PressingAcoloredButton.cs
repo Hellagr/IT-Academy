a8e374848208f3799f28c33e3dca5c46b9b6882d
Hellagr <alehkushniarou@gmail.com>
2024-12-31 00:11:58 +0300

    Interface implemented

--- Assets/Scripts/PressingAcoloredButton.cs
+++ Assets/Scripts/PressingAcoloredButton.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PressingAcoloredButton : ChangeColorButton
+public class PressingAcoloredButton : MonoBehaviour, IChangeColor
 {
+    [SerializeField] Material material;
+    Button button;
 
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ChangeColorOfShip);
+    }
+
+    public void ChangeColorOfShip() 
+    {
+        var currentShipId = GeneralDataManager.Instance.currentShip;
+        var ships = GeneralDataManager.Instance.Ships;
+        var currentShip = ships[currentShipId];
+        currentShip.GetComponent<MeshRenderer>().material = material;
+    }
 }
